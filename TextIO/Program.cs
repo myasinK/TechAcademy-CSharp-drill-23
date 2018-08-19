611e5e75c60526272e8f71ff31dc3248c1fef640
myasinK <yasinul@gmail.com>
2018-08-18 18:56:02 -0700

    Finish drill on text IO

---
+++ TextIO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Test text\nMore text";
+            Console.WriteLine("Please enter a number to save to file");
+            string num = Console.ReadLine();
+            string filePath = @"C:\Users\Yasin\log.txt";
+            File.WriteAllText(filePath, text + "\n" + num);
+
+            string readText = File.ReadAllText(filePath);
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Contents of text file:\n");
+            Console.WriteLine(readText);
+        }
+    }
+}
